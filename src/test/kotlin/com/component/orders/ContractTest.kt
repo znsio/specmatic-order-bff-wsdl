d57bbf6bfcd69cdecc8f1ec4c32d806c8ee4456c
Hari Krishnan <harikrishnan83@gmail.com>
2025-06-01 15:11:19 -0700

    Removing unused imports and variables

--- src/test/kotlin/com/component/orders/ContractTest.kt
+++ src/test/kotlin/com/component/orders/ContractTest.kt
@@ -11,26 +11,24 @@ import org.springframework.boot.test.context.SpringBootTest
 class ContractTests : SpecmaticContractTest {
 
     companion object {
-        private var httpStub: ContractStub? = null
+        private var wsdlStub: ContractStub? = null
         private const val APPLICATION_HOST = "localhost"
         private const val APPLICATION_PORT = "8080"
-        private const val HTTP_STUB_HOST = "localhost"
-        private const val HTTP_STUB_PORT = 8090
-        private const val ACTUATOR_MAPPINGS_ENDPOINT = "http://$APPLICATION_HOST:$APPLICATION_PORT/actuator/mappings"
+        private const val WSDL_STUB_HOST = "localhost"
+        private const val WSDL_STUB_PORT = 8090
 
         @JvmStatic
         @BeforeAll
         fun setUp() {
             System.setProperty("host", APPLICATION_HOST)
             System.setProperty("port", APPLICATION_PORT)
-            System.setProperty("endpointsAPI", ACTUATOR_MAPPINGS_ENDPOINT)
-            httpStub = createStub(host = HTTP_STUB_HOST, port = HTTP_STUB_PORT)
+            wsdlStub = createStub(host = WSDL_STUB_HOST, port = WSDL_STUB_PORT)
         }
 
         @JvmStatic
         @AfterAll
         fun tearDown() {
-            httpStub?.close()
+            wsdlStub?.close()
         }
     }
 }
